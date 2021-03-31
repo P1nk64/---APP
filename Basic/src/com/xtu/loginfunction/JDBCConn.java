58acb0cd2dca8e9bf45ea348e596482bfd918ff4
P1nk64's SurfacePro7 <yx.chn@foxmail.com>
2021-03-31 19:48:10 +0800

    提交登录与注册代码

--- Basic/src/com/xtu/loginfunction/JDBCConn.java
+++ Basic/src/com/xtu/loginfunction/JDBCConn.java
@@ -4,9 +4,10 @@ import com.xtu.utils.DBUtil;
 
 import java.sql.*;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class JDBCConn {
-    public static void main(String[] args) {
+    public static void SignUp(){
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -19,19 +20,69 @@ public class JDBCConn {
         try {
             conn = DBUtil.getConnection();
             String sql = "insert into user(user_id,user_name,user_pwd) values(?,?,?)";
+            Scanner input = new Scanner(System.in);
+            System.out.println("输入证件号");
+            String Suserid = input.nextLine();
+            int userid = Integer.parseInt(Suserid);
+            System.out.println("输入姓名");
+            String username = input.nextLine();
+            System.out.println("输入密码");
+            String userpwd = input.nextLine();
             ps = conn.prepareStatement(sql);
-            ps.setInt(1,3);
-            ps.setString(2,"Covena");
-            ps.setString(3,"998877");
+            ps.setInt(1,userid);
+            ps.setString(2,username);
+            ps.setString(3,userpwd);
             //4、执行SQL
             int count = ps.executeUpdate();
             System.out.println(count);
         } catch (Exception e) {
+            System.out.println("输入有误");
             e.printStackTrace();
         }finally {
             DBUtil.close(conn,ps,rs);
         }
-
-
     }
+
+    public static void SignIn(){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        //1、注册驱动
+        //Class.forName(driver);
+
+        //2、获取连接
+        // conn = DriverManager.getConnection(url,user,password);
+        try {
+            conn = DBUtil.getConnection();
+            String sql = "select *  from user Where (user_id = ? AND user_pwd = ?)";
+            Scanner input = new Scanner(System.in);
+            System.out.println("输入证件号");
+            String Suserid = input.nextLine();
+            int userid = Integer.parseInt(Suserid);
+
+            System.out.println("输入密码");
+            String userpwd = input.nextLine();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1,userid);
+
+            ps.setString(2,userpwd);
+            //4、执行SQL
+            ResultSet count = ps.executeQuery();
+
+            if(count.next()){
+                System.out.println("登录成功");
+            }
+            else {
+                System.out.println("登陆失败");
+            }
+            System.out.println(count);
+        } catch (Exception e) {
+            System.out.println("输入有误");
+            e.printStackTrace();
+        }finally {
+            DBUtil.close(conn,ps,rs);
+        }
+    }
+
 }
--- Basic/src/com/xtu/mainfunction/MainFuction.java
+++ Basic/src/com/xtu/mainfunction/MainFuction.java
@@ -1,8 +1,11 @@
 package com.xtu.mainfunction;
 import com.xtu.character.*;
+import com.xtu.loginfunction.JDBCConn;
 
 public class MainFuction {
     public static void main(String[] args) {
+        JDBCConn.SignUp();
+        JDBCConn.SignIn();
         ProcessFuction pf = new ProcessFuction();
     }
 }
